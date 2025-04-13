7dfa6d1fc942149ca607228dcdea6cf9a28eef51
Thaysa Miyasak Rezende <thaysaif2008@gmail.com>
2025-04-13 09:01:58 -0400

    Add files via upload

---
+++ ex27.py
@@ -0,0 +1,8 @@
+# 27. Lista de Nomes
+# - Crie uma lista chamada nomes e insira os nomes de 5 amigos.
+# - Use um laço for para exibir os nomes em ordem alfabética.
+
+nomes = ["Bela", "Arthur", "Anelize", "Renata", "Keylla"]
+nomes.sort()
+for i in range(0,5):
+    print (nomes[i])
